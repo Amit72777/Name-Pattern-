9bd7770cdcf5fbe683a95f7d10b53badf9533c98
Amit Kushwaha <145186713+Amit72777@users.noreply.github.com>
2023-12-18 21:25:38 +0530

    Update AMIT.py

--- AMIT.py
+++ AMIT.py
@@ -5,7 +5,7 @@ while i <=9:
     j = 1  # print pattern a 
     while j<=9:
         if i>=a and ( j==1 or i==a or j==n ) or ( j<=a and (i+j == a+1) or j>=a and j-i == a-1):
-            print("\4",end = "")
+            print("*",end = "")
         else :
             print(" ",end = "")
         j+=1
@@ -14,7 +14,7 @@ while i <=9:
     print("   ",end = "")
     while j<=9:
         if j ==1 or j==n or (i<=a and (i==j or i+j == n+1)):
-            print("\4",end = "")
+            print("*",end = "")
         else :
             print(" ",end = "")
         j+=1
@@ -23,7 +23,7 @@ while i <=9:
     print("   ",end = "")
     while j<=9:
         if i== 1 or i==n or j==a:
-            print("\4",end = "")
+            print("*",end = "")
         else :
             print(" ",end = "")
         j+=1
@@ -32,7 +32,7 @@ while i <=9:
     print("  ",end = "")
     while j<=9:
         if i==1 or j==a:
-            print("\4",end = "")
+            print("*",end = "")
         else :
             print(" ",end = "")
         j+=1
